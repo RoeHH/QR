78b54e187b05d532655b3b28debcf9d6f3878b42
iccee0 <git@roeh.ch>
2022-08-11 10:26:35 +0200

    const

--- main.tsx
+++ main.tsx
@@ -25,7 +25,7 @@ const handler = async (req: Request) => {
     })
   }
   if(url.slice(0,2) == "s/"){
-    { u, short }newShort(url.substring(2))
+    const { u, short }newShort(url.substring(2))
     url = "https://shorty.deno.dev/" + short
   }
   const qr = "data:image/png" + await qrcode(url).then(qr => qr.slice(14));
